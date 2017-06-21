07019ded8f5cbbc4b71fcb4b955924cda1f73be5
deeKodar <darpuntha02@gmail.com>
2017-06-21 16:23:32 +0600

    splash screen time fixed

--- app/src/main/java/bt/gov/dit/discoverbhutan/SplashActivity.java
+++ app/src/main/java/bt/gov/dit/discoverbhutan/SplashActivity.java
@@ -10,7 +10,7 @@ import android.os.Bundle;
 public class SplashActivity extends AppCompatActivity {
 
     Boolean loggedIn;
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
