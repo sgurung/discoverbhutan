b8a299e4f432801228d5f5a534e32f815badf4cd
deeKodar <darpuntha02@gmail.com>
2017-06-21 15:30:13 +0600

    splash screen added

---
+++ app/src/main/java/bt/gov/dit/discoverbhutan/SplashActivity.java
@@ -0,0 +1,51 @@
+package bt.gov.dit.discoverbhutan;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Boolean loggedIn;
+    private static int SPLASH_TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        SharedPreferences userprofile = this.getSharedPreferences(getString(R.string.app_name),MODE_PRIVATE);
+        loggedIn=userprofile.getBoolean("LoggedIn", false);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+
+                Intent i;
+                if(loggedIn==true) {
+
+
+                    i = new Intent(SplashActivity.this, MainActivity.class);
+
+
+
+                    finish();
+                } else {
+                    i = new Intent(SplashActivity.this, LoginActivity.class);
+                }
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
